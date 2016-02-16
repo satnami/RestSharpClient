e0782c74f60e875d5979ea8698424da6f23ce7fd
Sami Almouhtaseb <mssatnami@gmail.com>
2016-02-16 18:38:17 +0800

    migrate the project from tfs

---
+++ ClientRestSharp.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using ClientRestSharp.Engine;
+
+namespace ClientRestSharp.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var engine = new Client("http://localhost:8050");
+            System.Console.WriteLine(engine.Send());
+
+            while (true) if (System.Console.ReadLine() == "q") return;
+
+        }
+    }
+}
---
+++ ClientRestSharp.Engine/Client.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+using RestSharp;
+
+namespace ClientRestSharp.Engine
+{
+    public class Client
+    {
+        private readonly string _baseAddress;
+
+        public Client(string add)
+        {
+            _baseAddress = add;
+        }
+
+        public string Send()
+        {
+            var client = new RestClient(_baseAddress);
+            // client.Authenticator = new HttpBasicAuthenticator(username, password);
+
+            var request = new RestRequest("/", Method.GET);
+            //request.AddParameter("name", "value"); // adds to POST or URL querystring based on Method
+            //request.AddUrlSegment("id", "123"); // replaces matching token in request.Resource
+
+            // easily add HTTP Headers
+            request.AddHeader("header", "value");
+
+            IRestResponse response = client.Execute(request);
+            return response.Content;
+        }
+    }
+}
